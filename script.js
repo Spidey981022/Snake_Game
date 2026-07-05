9f9a5787bb29e8250971ea6df0d0856758dd886f
Tarun <17989458+Spidey981022@users.noreply.github.com>
2026-07-05 15:23:04 +0530

    snake moving all direction

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 const board = document.querySelector("#board");
 
 const blocks = [];
-const snake = [
-  { x: 1, y: 3 },
-  { x: 1, y: 4 },
-  { x: 1, y: 5 },
-];
+const snake = [{ x: 1, y: 3 }];
 
-let direction = "left";
+let direction = "down";
 
 const nCol = Math.floor(board.clientHeight / 50);
 const nRows = Math.floor(board.clientWidth / 50);
@@ -28,15 +24,23 @@ function render() {
   });
 }
 
-// setInterval(() => {
-//   let head = null;
-//   if (direction === "left") {
-//     head = { x: snake[0].x, y: snake[0].y - 1 };
-//   }
-//   snake.unshift(head);
-//   snake.forEach((segment) => {
-//     blocks[`${segment.x},${segment.y}`].classList.remove("fill");
-//   });
-//   snake.pop();
-//   render();
-// }, 1000);
+setInterval(() => {
+  let head = null;
+
+  if (direction === "right") {
+    head = { x: snake[0].x, y: snake[0].y + 1 };
+  } else if (direction === "left") {
+    head = { x: snake[0].x, y: snake[0].y - 1 };
+  } else if (direction === "up") {
+    head = { x: snake[0].x - 1, y: snake[0].y };
+  } else if (direction === "down") {
+    head = { x: snake[0].x + 1, y: snake[0].y };
+  }
+
+  snake.unshift(head);
+  snake.forEach((segment) => {
+    blocks[`${segment.x},${segment.y}`].classList.remove("fill");
+  });
+  snake.pop();
+  render();
+}, 1000);
